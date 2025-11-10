fe056732cd4ed473df73d4e554404469b479bc95
secretkota <stasbro123@gmail.com>
2025-11-10 13:07:49 +0200

    Feat: Add readme

--- server/middleware/authMiddleware.ts
+++ server/middleware/authMiddleware.ts
@@ -1,12 +1,26 @@
+import type { FastifyReply, FastifyRequest, HookHandlerDoneFunction } from 'fastify'
 import jwt from 'jsonwebtoken'
+import type { TUser } from '../types/user.types.ts';
 
-export function auth(request, reply, done) {
+declare module 'fastify' {
+  interface FastifyRequest {
+    user?: TUser;
+  }
+}
+
+export function auth(request: FastifyRequest, reply: FastifyReply, done: HookHandlerDoneFunction) {
     const authHeader = request.headers['authorization']
     if (!authHeader) return reply.code(401).send({ error: 'No token provided' })
-    const token = authHeader.split(' ')[1]
-    jwt.verify(token, process.env.SECRET_KET, (err, user) => {
-        if (err) return reply.code(403).send({ error: 'Invalid token' })
-        request.user = user
-        done()
-    })
+    const token = authHeader.split(' ')[1] as string
+    const SECRET_KEY = process.env['SECRET_KEY'] as string
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+        if (err) return reply.code(403).send({ error: 'Invalid token' });
+
+        if (!decoded || typeof decoded === 'string') {
+            return reply.code(403).send({ error: 'Invalid token payload' });
+        }
+
+        request.user = decoded as TUser;
+        return done();
+    });
 }
